e0407dde0937d658fc169bbde614322fd248a5b9
shannon <x.liu0779@gmail.com>
2023-10-24 13:50:30 +0800

    test: 💍 remove log

--- room.go
+++ room.go
@@ -87,8 +87,13 @@ func NewRoom(roomID string, handles ...func(message any)) (*Room, error) {
 }
 
 func (r *Room) Connect() error {
-	wsUrl := "wss://webcast3-ws-web-lq.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim|wss_push_room_id:%s|wss_push_did:%s|dim_log_id:202302171547011A160A7BAA76660E13ED|fetch_time:1676620021641|seq:1|wss_info:0-1676620021641-0-0|wrds_kvs:WebcastRoomStatsMessage-1676620020691146024_WebcastRoomRankMessage-1676619972726895075_AudienceGiftSyncData-1676619980834317696_HighlightContainerSyncData-2&cursor=t-1676620021641_r-1_d-1_u-1_h-1&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1&im_path=/webcast/im/fetch/&user_unique_id=%s&device_platform=web&cookie_enabled=true&screen_width=1440&screen_height=900&browser_language=zh&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/110.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai&identity=audience&room_id=%s&heartbeatDuration=0&signature=00000000"
-	wsUrl = strings.Replace(wsUrl, "%s", r.RoomId, -1)
+	wsUrl := "wss://webcast3-ws-web-lq.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim|wss_push_room_id:{ROOM_ID}|wss_push_did:{ROOM_ID}|dim_log_id:{DATATIME}1A160A7BAA76660E13ED|fetch_time:{TIMESTAMP}|seq:1|wss_info:0-{TIMESTAMP}-0-0|wrds_kvs:WebcastRoomStatsMessage-{TIMESTAMP}146024_WebcastRoomRankMessage-{TIMESTAMP}895075_AudienceGiftSyncData-{TIMESTAMP}317696_HighlightContainerSyncData-2&cursor=t-{TIMESTAMP}_r-1_d-1_u-1_h-1&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1&im_path=/webcast/im/fetch/&user_unique_id={ROOM_ID}&device_platform=web&cookie_enabled=true&screen_width=1440&screen_height=900&browser_language=zh&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/118.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai&identity=audience&room_id={ROOM_ID}&heartbeatDuration=0&signature=00000000"
+
+	currentTime := time.Now()
+	wsUrl = strings.Replace(wsUrl, "{ROOM_ID}", r.RoomId, -1)
+	wsUrl = strings.Replace(wsUrl, "{TIMESTAMP}", fmt.Sprintf("%d", currentTime.UnixNano()), -1)
+	wsUrl = strings.Replace(wsUrl, "{DATATIME}", currentTime.Format("20060102150405"), -1)
+
 	h := http.Header{}
 	h.Set("cookie", "ttwid="+r.Ttwid)
 	h.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
@@ -157,7 +162,7 @@ func (r *Room) send() {
 		if err != nil {
 			panic(err.Error())
 		}
-		log.Println("[系统] 发送 ping 包")
+		// log.Println("[系统] 发送 ping 包")
 		time.Sleep(time.Second * 10)
 	}
 }
@@ -172,7 +177,7 @@ func (r *Room) sendAck(logId uint64, iExt string) {
 	if err != nil {
 		panic(err.Error())
 	}
-	log.Println("[系统] 发送 ack 包")
+	// log.Println("[系统] 发送 ack 包")
 }
 
 func degzip(data []byte) ([]byte, error) {
@@ -201,7 +206,7 @@ func (r *Room) parseChatMsg(msg []byte) {
 func (r *Room) parseGiftMsg(msg []byte) {
 	giftMsg := &dyproto.GiftMessage{}
 	_ = proto.Unmarshal(msg, giftMsg)
-	// log.Printf("[礼物] %s : %s * %d \n", giftMsg.User.NickName, giftMsg.Gift.Name, giftMsg.ComboCount)
+	// log.Printf("[礼物] %s : %s * %d, %v, %v, 重复 %v \n", giftMsg.User.NickName, giftMsg.Gift.Name, giftMsg.ComboCount, giftMsg.Gift.Combo, giftMsg.SendTime, giftMsg.RepeatEnd)
 	for _, fn := range r.fns {
 		go fn(giftMsg)
 	}
